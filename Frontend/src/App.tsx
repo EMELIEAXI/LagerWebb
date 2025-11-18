78605b0e22cee1e047d3e82d5ff902847c2cf060
Emilia Almgren <emiliaalmgren@hotmail.com>
2025-11-18 16:17:41 +0100

    - Lagt till routing + (protectedRouting) + Autentication
     - Lagt till komponenter för inlogg, homepage, notfound
    
    - Tagit bort css-filer och lagt till EN global.css fil
    
    -

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,15 +1,43 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
+import NotFound from './pages/NotFound';
+import ProtectedRoute from './routes/ProtectedRoute';
+import NavMenu from './components/NavMenu';
+import { useAuth } from './context/AuthContext';
 
-function App() {
-  
+ function App() {
+  const { isAuthenticated } = useAuth();
 
   return (
     <>
-      <div>
-        <h1>Hello there!</h1>
-      </div>
+    {isAuthenticated && <NavMenu />}
+
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+
+      <Route 
+        path="/home" 
+        element={
+          <ProtectedRoute>
+            <HomePage />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route 
+        path="/notfound" 
+        element={
+          <ProtectedRoute>
+            <NotFound />
+          </ProtectedRoute>
+        }
+      />
+
+      <Route path="*" element={<LoginPage />} />
+    </Routes>
     </>
-  )
+  );
 }
 
 export default App
