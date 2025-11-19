8251085b122cfdcb9920b55c3c899d74582408f3
Emilia Almgren <emiliaalmgren@hotmail.com>
2025-11-19 12:05:08 +0100

    - NavMenu styleing + integrering av iconer och länkar

--- Frontend/src/components/NavMenu.tsx
+++ Frontend/src/components/NavMenu.tsx
@@ -1,14 +1,23 @@
 import { Link } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import styles from '../styles/NavMenu.module.css'
+import { Home, Wrench, History, Menu, X } from "lucide-react"
+import { useState } from "react";
 
 export default function NavMenu() {
   const { logout } = useAuth();
+  const [ open, setOpen ] = useState(false);
 
   return (
-    <nav>
-      <Link to="/home" >Hem</Link>
-      <Link to="/parts" >Reservdelar</Link>
-      <button onClick={logout}>Logga ut</button>
+    <>
+    <button onClick={logout} className={styles["logout-btn"]}>Logga ut</button>
+
+    <nav className={styles["nav-wrapper"]}>
+      <Link to="/home" className={styles["nav-link"]}><Home size={24}/>Home</Link>
+      <Link to="/parts" className={styles["nav-link"]}><Wrench size={24}/>Reservdelar</Link>
+      <Link to="/historik" className={styles["nav-link"]}><History size={24}/>Historik</Link>
+      <button className={styles["nav-link"]} onClick={() => setOpen(true)}><Menu size={28} /></button>
     </nav>
+    </>
   );
 }
